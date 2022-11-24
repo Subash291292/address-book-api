7fb697c62c233302c5a9f6c0be047fdb59a1c5f1
Subash291292 <118957895+Subash291292@users.noreply.github.com>
2022-11-24 22:07:25 +1100

    Add files via upload
    address book api initial version

---
+++ address-book-api/address-book-api/src/main/java/com/address/book/address/controller/AddressBookController.java
@@ -0,0 +1,47 @@
+package com.address.book.address.controller;
+
+import com.address.book.address.exception.InternalServerException;
+import com.address.book.address.model.AddressBook;
+import com.address.book.address.service.AddressBookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class AddressBookController {
+    
+    private Logger log = LoggerFactory.getLogger(AddressBookController.class);
+    @Autowired
+    private AddressBookService addressBookService;
+    
+    @PostMapping("/api/v1/address-book/{addressBookId}/contacts")
+    public ResponseEntity<AddressBook> saveAddressBook(@PathVariable String addressBookId, @Valid @RequestBody AddressBook book) throws InternalServerException {
+        try {
+            log.info("saveAddressBook method invoked for this address : " + addressBookId);
+            addressBookService.saveAddressBook(addressBookId, book);
+            return new ResponseEntity<AddressBook>(book, HttpStatus.CREATED);
+        } catch (Exception e) {
+            log.error("Exception occurred in saveAddressBook method  : ", e);
+            throw new InternalServerException("Error while saving the Address Book request");
+        }
+    }
+    
+    @GetMapping("/api/v1/address-book/contacts")
+    public ResponseEntity<List<AddressBook>> findAddressBookList(@RequestParam(value = "unique", defaultValue = "false") final Boolean unique) throws InternalServerException {
+        try {
+            log.info("findFriendsList method invoked");
+            List<AddressBook> friendsList = addressBookService.findAddressBookList(unique);
+            return new ResponseEntity<>(friendsList, HttpStatus.OK);
+        }
+        catch (Exception e) {
+            log.error("Exception block  : ", e);
+            throw new InternalServerException("Error while processing request");
+        }
+    }
+}
