daebfb19f5ab465ea564a85efbbf87affb26f284
Subash291292 <118957895+Subash291292@users.noreply.github.com>
2022-11-24 22:10:16 +1100

    address book api
    Initial Version of address book api

---
+++ src/main/java/com/address/book/address/controller/advice/AddressBookExceptionController.java
@@ -0,0 +1,24 @@
+package com.address.book.address.controller.advice;
+
+import com.address.book.address.model.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class AddressBookExceptionController {
+    
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    @ExceptionHandler(value = {InternalError.class})
+    public ResponseEntity handleInternalError() {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorResponse.setMessage("Error while processing the request");
+        errorResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.name());
+        return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ src/main/java/com/address/book/address/model/ErrorResponse.java
@@ -0,0 +1,35 @@
+package com.address.book.address.model;
+
+public class ErrorResponse {
+    
+    private int code;
+    private String status;
+    private String message;
+    
+    public ErrorResponse() {
+    }
+    
+    public int getCode() {
+        return this.code;
+    }
+    
+    public void setCode(int code) {
+        this.code = code;
+    }
+    
+    public String getStatus() {
+        return this.status;
+    }
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    public String getMessage() {
+        return this.message;
+    }
+    
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
